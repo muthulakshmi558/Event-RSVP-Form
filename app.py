8dc9394b3be770465e06389bf298dcf294fb5896
Muthulakshmi <muthulakshmi5293@gmail.com>
2025-08-12 23:10:01 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,30 @@
 from flask import Flask, render_template, flash, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, Email, NumberRange, Length
+from wtforms import StringField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email
 import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
 
-class ReviewForm(FlaskForm):
+# RSVP Form Class
+class RSVPForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired(), Email()])
-    rating = IntegerField("Rating (1-5)", validators=[DataRequired(), NumberRange(min=1, max=5, message="Rating must be between 1 and 5")])
-    review = TextAreaField("Review Message", validators=[DataRequired(), Length(min=10)])
-    submit = SubmitField("Submit Review")
+    will_attend = BooleanField("I will attend")
+    submit = SubmitField("Submit RSVP")
 
 @app.route("/", methods=["GET", "POST"])
-@app.route("/review", methods=["GET", "POST"])
-def review():
-    form = ReviewForm()
+@app.route("/rsvp", methods=["GET", "POST"])
+def rsvp():
+    form = RSVPForm()
     if form.validate_on_submit():
-        flash("Thank you for your review!", "success")
-        return redirect(url_for("review"))
-    return render_template("review.html", form=form)
+        if form.will_attend.data:
+            flash("Great! We’re excited to see you at the event.", "success")
+        else:
+            flash("Sorry to miss you! Maybe next time.", "error")
+        return redirect(url_for("rsvp"))
+    return render_template("rsvp.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
